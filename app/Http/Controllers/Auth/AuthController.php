b01a704f4fe58328180ea845db8ea53f4d6f88f4
Lino Puma Ticona <nearlino20@gmail.com>
2023-05-23 15:52:42 -0500

    guest constructor

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -8,6 +8,12 @@ use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
 {
+
+    public function __construct()
+    {
+        $this->middleware('guest')->except('logout');
+    }
+    
     public function signIn(Request $request)
     {
         $this->validate($request, [
